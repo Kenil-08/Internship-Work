1e550bc6eab6d187745b68f366da3989dfe5431f
kenil-08 <kenilkanani09@gmail.com>
2024-07-19 22:48:02 +0530

    Login-Signup Perfect

--- file-transfer-app/server/index.js
+++ file-transfer-app/server/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser')
-const {connectMongoDb} = require('./connection');
-const userRoute = require('./routes/user');
-const staticRoute = require('./routes/staticRouter');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const { connectMongoDb } = require("./connection");
+const userRoute = require("./routes/user");
+const staticRoute = require("./routes/staticRouter");
 // const User = require('./models/user');
 const dotenv = require("dotenv");
 
@@ -13,11 +13,11 @@ const app = express();
 // NOTE : Provide a Database URL.
 connectMongoDb(process.env.MONGODB_URI);
 
-app.set('view engine','ejs');
-app.set('views', path.resolve('./views'))
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.get('/test', async(req,res)=>{
 //     const allUsers = await User.find({});
@@ -27,10 +27,9 @@ app.use(cookieParser());
 //     });
 // });
 
-app.use('/user',userRoute);
-app.use('/',staticRoute);  
+app.use("/user", userRoute);
+app.use("/", staticRoute);
 
-
-app.listen(3000,()=>{
-    console.log('Server Started on PORT : 3000!');
+app.listen(3000, () => {
+  console.log("Server Started on PORT : 3000!");
 });
